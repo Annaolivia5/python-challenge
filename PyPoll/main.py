c736c706d08b0a97c5e0528c3948283a915cceab
annaweeks <aoweeks@yahoo.com>
2021-08-24 21:53:03 -0700

    Added PyPoll main.py

---
+++ PyPoll/main.py
@@ -0,0 +1,78 @@
+import os
+import csv
+
+# File to read from
+election_data_path = os.path.join('.', 'Resources', 'election_data.csv')
+
+# Calculate the Following
+total_votes_cast = 0
+candidates = {}
+winner = ""
+winner_vote_count = 0
+
+# read each row of data
+with open(election_data_path, 'r') as csvfile:
+    
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    header = next(csvreader)
+
+    for candidate in csvreader:
+
+        total_votes_cast += 1
+
+        candidate_name = candidate[2]
+        
+        # add candidate and their total votes to the dictionary
+        if(candidate_name in candidates):
+            candidates[candidate_name] = (candidates[candidate_name] + 1)             
+        else:
+            candidates[candidate_name] = 1     
+
+
+for candidate in candidates:
+
+    # check to see if candidate is winning
+    if(candidates[candidate] > winner_vote_count):
+        winner_vote_count = candidates[candidate] 
+        winner = candidate
+
+    # calculate the % of votes for each candidate and update the dictionary with the percentages 
+    candidates[candidate] = candidates[candidate]/total_votes_cast
+
+# print the analysis
+print("Election Results\n-------------------------\n")
+print(f"Total Votes: {total_votes_cast}")
+print("-------------------------")
+
+for candidate in candidates:
+    percentage_won = round((candidates[candidate] * 100),3)
+    print(f"{candidate}: {percentage_won}%")
+
+print("-------------------------")
+print(f"Winner: {winner}")
+print("-------------------------")
+
+#-----------------------------------------------------------------------------------------
+# Export the analysis to a text file.
+
+# Set variable for output file
+output_file = os.path.join('.', 'analysis', 'PyPollAnalysis.txt')
+
+#  Open the output file
+with open(output_file, "w", newline="") as datafile:
+    
+    datafile.write("Election Results\n-------------------------\n")
+    datafile.write(f"Total Votes: {total_votes_cast}\n")
+    datafile.write("-------------------------\n")
+
+    for candidate in candidates:
+        percentage_won = round((candidates[candidate] * 100),3)
+        datafile.write(f"{candidate}: {percentage_won}%\n")
+ 
+    datafile.write("-------------------------\n")
+    datafile.write(f"Winner: {winner}\n")
+    datafile.write("-------------------------")
+    
+
+ 
